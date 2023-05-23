4e6320c07c2a56345abdced0c194cf0a623c02f4
whyest <whyest@gmail.com>
2023-05-23 11:36:14 +0300

    added sign in page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import React from 'react'
 import './App.css'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages'
+import SigninPage from './pages/signin'
 
 function App() {
   return (
     <Router>
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/signin' element={<SigninPage />} />
+      </Routes>
     </Router>
   )
 }
